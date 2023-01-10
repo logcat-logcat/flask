86135f5559e9c2dd1235996a5cc01368f773f0f3
logcat <sdh9089@naver.com>
2023-01-10 20:13:57 +0900

    1

---
+++ voca.py
@@ -0,0 +1,22 @@
+﻿from flask import Flask
+from flask import render_template
+from flask import request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index_page():
+    return render_template('voca.html')
+
+@app.route('/login', methods = ['POST'])
+def login():
+	
+	id = request.form['id']
+	pas = request.form['pass']
+	
+	return "id : " + id + ", " + "pass : " + pas
+
+if __name__ == '__main__':
+    app.run(host='210.114.22.121', port = 5550)
